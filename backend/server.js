e0cfa6527ef67f65c50ffd5135981b09ad3a3bb3
BinulaDimantha <habdthilakasiri2002@gmail.com>
2025-07-17 17:08:52 +0530

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -144,7 +144,7 @@ app.use('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Database connection and server start
 const startServer = async () => {
