ab63c9a5b2e81b0d10f6cefd7b5e416e8de72291
ravindugamage168 <ravindugamage168@gmail.com>
2025-07-22 17:28:52 +0530

    Port fix ******

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5005';
+
 
 const api = axios.create({
-  baseURL: `${API_URL}/api`,
+  baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
